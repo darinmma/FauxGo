8269a3e2537507255330ca9167943951268fd696
Darin Ma <darinmma@gmail.com>
2016-10-12 12:03:24 -0700

    xy plotting for win logic

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@ var currentTurn = 'B';
 var playable = $('.playable');
 console.log(playable);
 
+//plays pieces on the board, alternating turns
+
 playable.on('click', function() {
   if($(this).html() == '') {
     console.log("you clicked on", this
@@ -21,16 +23,17 @@ playable.on('click', function() {
       $(this).addClass('white')
       $(this).removeClass('unplayed')
     }
+    createNeighbors(this)
     switchTurn()
   } else {
     alert('Please replay your turn');
   }
-  checkPrisoner(parseInt(this.id));
+  // checkPrisoner($(this).attr('id'));
   console.log('check for prisoners');
-  //checkWin();
-  // switchTurn();
+  // checkWin();
 })
 
+//switch turns callback
 
 function switchTurn() {
   if(currentTurn == 'B') {
@@ -40,31 +43,83 @@ function switchTurn() {
   }
 }
 
-function checkPrisoner(cellId) {
-  // for($('#11')) {
-  //   if (currentTurn = 'W' && $('#21').hasClass('white') && $('#12').hasClass('white') {
-  //     $('#11').removeClass('black white playable')
-  //   } else if (currentTurn = 'B' && $('#21').hasClass('black') && $('#12').hasClass('black') {
-  //     $('#11').removeClass('black white playable')
-  //   }
-  if(currentTurn =='W' && ($('#'+(cellId + 10)).hasClass('white') || $('#'+(cellId - 10)).hasClass('white') || $('#'+(cellId + 1)).hasClass('white') || $('#'+(cellId - 1)).hasClass('white')) ) {
-    console.log(typeof(cellId));
-    console.log($('#'+(cellId + 10)));
-    console.log($('#'+(cellId - 10)));
-    console.log($('#'+(cellId + 1)));
-    console.log($('#'+(cellId - 1)));    console.log('possible white prisoners');
-  } else if (currentTurn =='B' && ($('#'+(cellId + 10)).hasClass('black') || $('#'+(cellId - 10)).hasClass('black') || $('#'+(cellId + 1)).hasClass('black') || $('#'+(cellId - 1)).hasClass('black'))) {
-    console.log(typeof(cellId));
-    console.log($('#'+(cellId + 10)));
-    console.log($('#'+(cellId - 10)));
-    console.log($('#'+(cellId + 1)));
-    console.log($('#'+(cellId - 1)));    console.log('possible black prisoners');
+//create neighbors array for single capture comparison
+
+function createNeighbors(self) {
+  var neighbors = []
+  var coord = $(self).attr('id').split('')
+  console.log(coord)
+  var yCoord = parseInt(coord[0])
+  var xCoord = parseInt(coord[1])
+  console.log(yCoord)
+  console.log(xCoord)
+  if (yCoord > 1) {
+    var above = [(yCoord-1), xCoord].join('')
+    neighbors.push(above)
+  }
+  if (xCoord < 9) {
+    var right = [yCoord,(xCoord + 1)].join('')
+    neighbors.push(right)
+  }
+  if (yCoord < 9) {
+    var below = [(yCoord+1),xCoord].join('')
+    neighbors.push(below)
+  }
+  if (xCoord > 1) {
+    var left = [yCoord,(xCoord - 1)].join('')
+    neighbors.push(left)
+    console.log(neighbors)
+    checkNeighbors(neighbors,self)
   }
 }
-//    ($('#'+(cellId + 10)).hasClass('white') || $('#'+(cellId - 10)).hasClass('white') || $('#'+(cellId + 1)).hasClass('white') || $('#'+(cellId - 1)).hasClass('white')) {
-//     console.log('possible prisoners')
-//   }
+
+function checkNeighbors(neighborsArr,playedDiv) {
+  for(var i = 0; i < neighborsArr.length; i++) {
+    if (currentTurn == 'B' && $(neighborsArr).eq(i).hasClass('white')){
+      var captureArray = []
+      var captureCoord = $(neighborsArr).eq(i).attr('id').split('')
+      console.log(captureCoord)
+      var yCoord = parseInt(coord[0])
+      var xCoord = parseInt(coord[1])
+      console.log(coord)
+      if (yCoord > 1) {
+        var above = [(yCoord-1), xCoord].join('')
+        captureArray.push(above)
+      }
+      if (xCoord < 9) {
+        var right = [yCoord,(xCoord + 1)].join('')
+        captureArray.push(right)
+      }
+      if (yCoord < 9) {
+        var below = [(yCoord+1),xCoord].join('')
+        captureArray.push(below)
+      }
+      if (xCoord > 1) {
+        var left = [yCoord,(xCoord - 1)].join('')
+        captureArray.push(left)
+        console.log(captureArray)
+        checkTaken(captureArray, possPrisoner)
+    }
+  }
+}
+}
+
+// function checkNeighbors(neighborsArr,playedSpot) {
+//   if (currentTurn === 'B')
+//   for(var i = 0; i < neighborsArr.length; i++) {
+//     if($('#' + neighborsArr[i]).hasClass('white')){
+//       // $(playedSpot).removeClass('black')
+//       $(playedSpot).addClass('taken')
+//     } else if(currentTurn === 'W')
+//       for(var i = 0; i < neighborsArr.length; i++) {
+//         if($('#' + neighborsArr[i]).hasClass('black')){
+//           // $(playedSpot).removeClass('white')
+//           $(playedSpot).addClass('taken')
+//         }
+//     }
 // }
+// }
+// reset button for game
 
 var resetButton = $('#resetGame');
 
